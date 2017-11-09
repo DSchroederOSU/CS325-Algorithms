4a4d2152d67b46bf58c1de23827735c5ec0a4f82
DSchroederOSU <schrodan@oregonstate.edu>
2017-11-09 12:04:34 -0800

    group assignment 3

---
+++ Minimal Spanning Tree/MST.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# importing "heapq" to implement heap queue
+import heapq
+
+
+def Jurnik(G, w):
+
+    # T - set of MST vertices
+    # V - any vertex
+    # H - empty heap
+
+    # Insert all v-edges into H
+
+    # initializing list
+    li = [5, 7, 9, 1, 3]
