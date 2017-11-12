feeb03af9d4f246b5c4f6c6dbae22fd21e9d6b8f
DSchroederOSU <schrodan@oregonstate.edu>
2017-11-12 13:41:13 -0800

    comments

--- GroupAssignment3/mst.py
+++ GroupAssignment3/mst.py
@@ -12,7 +12,6 @@ with open('input.txt', 'r') as f:
     n = int(f.readline())
     adjMatrix = [line.strip('\n').split(',') for line in f]
     adjMatrix = [[int(y) for y in x] for x in adjMatrix]
-
     f.close()
 
 
@@ -22,41 +21,42 @@ def heapInsert(v, arr, heap):
             heapq.heappush(heap, (arr[i], (v, i)))
 
 
-# T - set of MST vertices
-# V - any vertex
-# H - empty heap
 
-Tone = 0
+firstMSTtotal = 0
 lastAdded = 0
 # initializing list
 myHeap = []
 label = [0 for x in range(n)]
-print(label)
 # using heapify to convert list into heap
 heapq.heapify(myHeap)
 
+# insert the first vertex into the heap with all of its its edges
+# i.e. the first row if the adjacency matrix
 heapInsert(0, adjMatrix[0], myHeap)
+
+# update label array
 label[0] = 1
-print(myHeap)
-
-while len(myHeap) > 0:  # while heap is not empty
-    (weight, (v, u)) = heapq.heappop(myHeap)
-    if label[u] != label[v] and label[v] == 1:
-        label[u] = 1
-        lastAdded = (weight, u)
-        heapInsert(u, adjMatrix[u], myHeap)
-        Tone += weight
-    elif label[u] != label[v] and label[u] == 1:
-        label[v] = 1
-        lastAdded = (weight, v)
-        heapInsert(v, adjMatrix[v], myHeap)
-        Tone += weight
-    print(Tone)
-    print(myHeap)
 
 
-print(list(myHeap))
-print(list(myHeap))
+# while heap is not empty
+while len(myHeap) > 0:
+    (weight, (v, u)) = heapq.heappop(myHeap)  # pop the minimum weight edge (root) from heap
+
+    #  look at both vertices of the edge we just pulled from the heap
+    if label[u] != label[v] and label[v] == 1:  # if the vertex 'v' is already in MST
+        label[u] = 1  # add 'u' to the MST
+        lastAdded = (weight, u)  # update the lastAdded variable
+        heapInsert(u, adjMatrix[u], myHeap)  # add all the edges of the vertex we added into heap
+        firstMSTtotal += weight  # add the weight to MST total weight
+    elif label[u] != label[v] and label[u] == 1:  # if the vertex 'u' is already in MST
+        label[v] = 1  # add 'v' to the MST
+        lastAdded = (weight, v)  # update the lastAdded variable
+        heapInsert(v, adjMatrix[v], myHeap)  # add all the edges of the vertex we added into heap
+        firstMSTtotal += weight  # add the weight to MST total weight
+
+f = open("output.txt", "w+")
+f.write(str(firstMSTtotal))
+
 exit(0)
 
 
