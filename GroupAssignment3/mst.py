1eaa7b7257ba11c74a7c05a02bbdaa266e0cf93c
DSchroederOSU <schrodan@oregonstate.edu>
2017-11-13 21:59:33 -0800

    fixed it. flawless baby

--- GroupAssignment3/mst.py
+++ GroupAssignment3/mst.py
@@ -26,14 +26,16 @@ def readRow(row):
 
 
 
-def primMST(heap, e):
-    (w, (v1, v2)) = e
+def primMST(heap, e, last_edge):
+    (w, (v1, u1)) = e
+    (w2, (v2, u2)) = last_edge
     MSTtotal = 0
     # while heap is not empty
 
     while len(heap) > 0:
         (weight, (v, u)) = heapq.heappop(heap)  # pop the minimum weight edge (root) from heap
-        if v1 != v or v2 != u:
+
+        if (v1 != v or u1 != u) and (v2 != v or u2 != u):
             #  look at both vertices of the edge we just pulled from the heap
             if label[u] != label[v] and label[v] == 1:  # if the vertex 'v' is already in MST
                 label[u] = 1  # add 'u' to the MST
@@ -64,7 +66,7 @@ heapInsert(0, readRow(1), myHeap)
 # update label array
 label[0] = 1
 
-first_mst = primMST(myHeap, (-1, (-1, -1)))
+first_mst = primMST(myHeap, (-1, (-1, -1)), (-1, (-1, -1)))
 f = open("output.txt", "w+")
 f.write(str(first_mst[0]) + '\n')
 
@@ -88,7 +90,8 @@ for bad_edge in first_mst[1]:
 
     # update label array
     label[0] = 1
-    second_MST = primMST(myHeap, bad_edge)
+
+    second_MST = primMST(myHeap, bad_edge, (-1, (-1, -1)))
     if min == -1 or second_MST[0] < min:
         min = second_MST[0]
         MSTedgesMin = second_MST[1]  # store the edges of the min tree we're gonna keep
@@ -96,13 +99,29 @@ for bad_edge in first_mst[1]:
 
 f.write(str(min) + '\n')
 
+if n > 3:
+    s = set(first_mst[1])
+    new_edge = [x for x in MSTedgesMin if x not in s]
+
+    for edge in new_edge:
+        (w1, (v1, u1)) = edge
+        for check in first_mst[1]:
+            (w, (v, u)) = check
+            if (v1 == v and u1 == u) or (u1 == v and v1 == u):
+                new_edge.remove(edge)
+else:
+    new_edge = [(-1, (-1, -1))]
+
+
 #  ------------------------------------------------------------
 #  -------------------- FIND THE THIRD MST --------------------
 #  ------------------------------------------------------------
 
 secondMSTtotal = min
 min = -1
-for bad_edge in MSTedgesMin:
+MSTedgesMintwo = []
+for bad_edge in first_mst[1]:
+
     myEdges = []
     # initializing list
     myHeap = []
@@ -117,11 +136,11 @@ for bad_edge in MSTedgesMin:
 
     # update label array
     label[0] = 1
-    third_MST = primMST(myHeap, bad_edge)
-    if (min == -1 or third_MST[0] < min) and third_MST[0] >= secondMSTtotal:
-        min = third_MST[0]
-        MSTedgesMin = second_MST[1]  # store the edges of the min tree we're gonna keep
+    third_MST = primMST(myHeap, bad_edge, new_edge[0])
 
+    if (min == -1 or third_MST[0] < min) and third_MST[0] > secondMSTtotal:
+        min = third_MST[0]
+        MSTedgesMintwo = second_MST[1]  # store the edges of the min tree we're gonna keep
 
 f.write(str(min) + '\n')
 
